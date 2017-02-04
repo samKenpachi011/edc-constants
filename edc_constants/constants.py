617701acdaed7a422a993601676d75b4972ccece
erikvw <ew2789@gmail.com>
2017-02-04 12:09:59 +0200

    add NONE

--- edc_constants/constants.py
+++ edc_constants/constants.py
@@ -30,6 +30,7 @@ NEG = 'NEG'
 NEVER = 'NEVER'
 NEW = 'New'
 NO = 'No'
+NONE = 'none'
 NOT_ADDITIONAL = False
 NOT_APPLICABLE = 'N/A'
 NOT_EVALUATED = 'Not evaluated'
@@ -56,7 +57,8 @@ SUBJECT = 'subject'
 UNK = 'UNK'
 UNKNOWN = 'unknown'
 UPDATE = 'UPDATE'
-UUID_PATTERN = re.compile('[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}')
+UUID_PATTERN = re.compile(
+    '[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}')
 VIEW = 'VIEW'
 WEEKDAYS = 'weekdays'
 WEEKENDS = 'weekends'
