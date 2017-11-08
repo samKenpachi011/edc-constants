c350b21f4ed2f110c8197c034a4ba0a9fdc2b418
erikvw <ew2789@gmail.com>
2017-11-08 10:58:56 +0200

    remove pharmacy specific choices

--- edc_constants/choices.py
+++ edc_constants/choices.py
@@ -97,30 +97,6 @@ DEATH_RELATIONSIP_TO_STUDY = (
     ('Definitely related', 'Definitely related')
 )
 
-DRUG_FORMULATION = (
-    ('11', 'Tablet'),
-    ('12', 'Capsule'),
-    ('13', 'Liquid'),
-    ('14', 'Powder'),
-    ('15', 'Suspension'),
-    ('16', 'Gel'),
-    ('17', 'Oil'),
-    ('18', 'Lotion'),
-    ('19', 'Cream'),
-    ('20', 'Patch'),
-    ('99', 'Other'),
-)
-
-DRUG_ROUTE = (
-    ('1', 'Intramuscular'),
-    ('2', 'Intravenous'),
-    ('3', 'Oral'),
-    ('4', 'Topical'),
-    ('5', 'Subcutaneous'),
-    ('6', 'Intravaginal'),
-    ('7', 'Rectal'),
-    ('9', 'Other'),
-)
 
 FEEDING = (
     ('BF', 'Breast Feed'),
